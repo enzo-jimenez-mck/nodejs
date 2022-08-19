8bccbe785a6e00f4cf5e36368d331a02b3a30bd8
Enzo Jimenez <83845663+enzo-jimenez-mck@users.noreply.github.com>
2022-08-18 21:57:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 80;
+const http = require('http');
 
-app.get('/', (req,res) => {
-  console.log('get');
-    res.send('<img src="https://raw.githubusercontent.com/enzo-jimenez-mck/nodejs/blob/main/k8s.jfif" />');
-});
-
-app.listen(PORT, () => console.log(`Listening ${PORT}`));
+http.createServer(function (req, res) {
+  res.write('<img src="https://raw.githubusercontent.com/enzo-jimenez-mck/nodejs/blob/main/k8s.jfif" />');
+  res.end();
+}).listen(8080);
